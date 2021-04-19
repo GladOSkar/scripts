d6e7207023a5c7cd5628c90c5b3742f08f2dd264
Oskar <oskar@winkels.berlin>
2021-04-19 17:25:32 +0200

    Add parameter for screen configuration

--- enable-external-monitor.sh
+++ enable-external-monitor.sh
@@ -7,15 +7,18 @@ MONITOR2=`xrandr | grep " connected " | grep -v eDP | tail -n 1 | cut -d ' ' -f 
 if [[ $MONITOR1 ]] && [[ $MONITOR1 != $MONITOR2 ]]
 then
 	echo 2 Monitors
-	# T-Formation: 2 externals next to each other on top, internal in the middle below
-	xrandr	--output $INTERNAL --primary --auto --pos 960x1080 \
-			--output $MONITOR1 --auto --pos 0x0 \
-			--output $MONITOR2 --auto --pos 1920x0
-
-	# 2 Vertical 1920x1200s
-#	xrandr	--output $INTERNAL --primary --auto --pos 600x1920 \
-#			--output $MONITOR1 --auto --pos 0x0 --rotate left \
-#			--output $MONITOR2 --auto --pos 1200x0 --rotate left
+	if [[ $1 == "--vertical" ]]
+	then
+		 # 2 Vertical 1920x1200s
+		xrandr	--output $INTERNAL --primary --auto --pos 600x1920 \
+				--output $MONITOR1 --auto --pos 0x0 --rotate left \
+				--output $MONITOR2 --auto --pos 1200x0 --rotate left
+	else
+		# T-Formation: 2 externals next to each other on top, internal in the middle below
+		xrandr	--output $INTERNAL --primary --auto --pos 960x1080 \
+				--output $MONITOR1 --auto --pos 0x0 \
+				--output $MONITOR2 --auto --pos 1920x0
+	fi
 elif [[ $MONITOR1 ]]
 then
 	echo 1 Monitor
